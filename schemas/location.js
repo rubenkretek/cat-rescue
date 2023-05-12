e7a87e3c3e74002fdadbc089ca89106bdcc2e5ec
rubenkretek <rubenkretek@outlook.com>
2023-05-12 16:47:12 +0100

    Added more fields to location

--- schemas/location.js
+++ schemas/location.js
@@ -6,9 +6,54 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'city',
-      title: 'City',
+      name: 'address1',
+      title: 'Address Line 1',
       type: 'string',
+    }),
+    defineField({
+      name: 'address2',
+      title: 'Address Line 2',
+      type: 'string',
+    }),
+    defineField({
+      name: 'city',
+      title: 'Town/City',
+      type: 'string',
+    }),
+    defineField({
+      name: 'county',
+      title: 'County',
+      type: 'string',
+    }),
+    defineField({
+      name: 'postcode',
+      title: 'Postcode',
+      type: 'string',
+    }),
+    defineField({
+      name: 'mapsURL',
+      title: 'Google Maps link',
+      type: 'url',
+    }),
+    defineField({
+      name: 'email',
+      title: 'Email Address',
+      type: 'string',
+    }),
+    defineField({
+      name: 'phone',
+      title: 'Phone Number',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
     }),
     defineField({
       name: 'slug',
@@ -18,11 +63,6 @@ export default defineType({
         source: 'city',
         maxLength: 96,
       },
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
     }),
   ],
 })
